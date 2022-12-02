da7fb819a670e8038c4cf8a195fac7f2a2fb84cc
LEE SUHONG <tnghd222@naver.com>
2022-12-02 16:22:12 +0900

    제너릭

---
+++ Day16/src/generic/StudentVO.java
@@ -0,0 +1,9 @@
+package generic;
+
+public class StudentVO {
+
+		String hak;
+		String name;
+		String department;
+	
+}
---
+++ Day16/src/generic/Test.java
@@ -0,0 +1,12 @@
+package generic;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ArrayList<StudentVO> st = new ArrayList<StudentVO>(); 
+		st.add(null);
+	}
+}
