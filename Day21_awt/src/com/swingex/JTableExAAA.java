b83be436a4697553df2be45511802bea4fadddba
LEE SUHONG <tnghd222@naver.com>
2022-12-30 16:07:54 +0900

    Swing 수업 내용

---
+++ Day21_awt/src/com/swingex/JTableExAAA.java
@@ -0,0 +1,33 @@
+package com.swingex;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class JTableExAAA {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame("참가사 명단 프로그램");
+		
+		f.setPreferredSize(new Dimension(300,150));
+		f.setLocation(500, 400);
+		Container con = f.getContentPane();
+		
+		String colName[] = {"이름", "나이", "성별"};
+		Object data[][] = {
+				{"김철수", 23, "男"},
+				{"김영희",24,"女"},
+				{"홍길동",25,"男"}
+		};
+		
+		JTable table = new JTable(data, colName);
+		
+		JScrollPane scrollPane = new JScrollPane(table);
+		con.add(scrollPane, BorderLayout.CENTER);
+		JButton Button = new JButton("출력");
+		Button.addActionListener(new PrintActionListenerAAA(table));
+		
+		con.add(Button, BorderLayout.SOUTH);
+		f.pack();
+		f.setVisible(true);
+	}
+}
